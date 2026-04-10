e5add6dd8a1486ff3059482b328b0b9e52240d83
Jesus <jcampoy21@campusuninunez.edu.co>
2026-04-10 12:36:39 -0500

    Version 1.2

---
+++ CPP_Project/ProyectoIntegrador.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Estudiante {
+public:
+    std::string nombre;
+    // Matriz de 3x3: [Materias][Cortes]
+    double notas[3][3];
+
+    Estudiante(std::string n) : nombre(n) {
+        // Inicializar matriz en 0
+        for(int i=0; i<3; i++)
+            for(int j=0; j<3; j++) notas[i][j] = 0.0;
+    }
+
+    void mostrarRecord Academico() {
+        std::cout << "\nNotas de " << nombre << ":" << std::endl;
+        for(int i=0; i<3; i++) {
+            std::cout << "Materia " << i+1 << ": ";
+            for(int j=0; j<3; j++) {
+                std::cout << "[" << notas[i][j] << "] ";
+            }
+            std::cout << std::endl;
+        }
+    }
+};
+
+int main() {
+    // Arreglo de objetos
+    std::vector<Estudiante> curso;
+    curso.push_back(Estudiante("Jesus"));
+
+    // Llenar una nota específica en la matriz del objeto
+    curso[0].notas[0][0] = 4.5; // Materia 1, Corte 1
+    curso[0].notas[1][2] = 3.8; // Materia 2, Corte 3
+
+    curso[0].mostrarRecordAcademico();
+
+    return 0;
+}
